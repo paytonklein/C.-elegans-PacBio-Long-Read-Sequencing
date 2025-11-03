3e72415f8f3b008faa60c0d1d52642ff65a0c033
paytonklein <165186746+paytonklein@users.noreply.github.com>
2025-11-03 13:34:29 -0500

    multiprocessing of indel stats

--- python_scripts/indels_stats.py
+++ python_scripts/indels_stats.py
@@ -5,7 +5,9 @@ from collections import defaultdict
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-# test
+
+# multiprocessing packages
+from multiprocessing import Pool
 
 def GetGeneAccession(variant, info_elements):
     accession = 'NA'
@@ -22,34 +24,25 @@ def GetGeneAccession(variant, info_elements):
                         accession = a.split('|')[6]
     return accession
 
-
-if __name__ == "__main__":
-    vcf_file = sys.argv[1]
-    sample = sys.argv[2]
-    variant_size_threshold = int(sys.argv[3])
-    output_dir = sys.argv[4]
-
-    # create output summary files
-    del_out = open(os.path.join(output_dir, f'{sample}_deletions_{variant_size_threshold}_info.tsv'), 'w')
-    ins_out = open(os.path.join(output_dir, f'{sample}_insertions_{variant_size_threshold}_info.tsv'), 'w')
+# process one sample VCF and return variant data/stats
+def ProcessSample(args):
+    vcf_file, sample, output_dir = args
     
-    # store size of insertions and deletions
+    # store the size of indels, number of multiple variants and number of variants
     insertions = []
     deletions = []
-    # store the number of multiple variants
     mul_variants = 0
-    # store the number of variants
     num_variants = 0
+    summary_records = []
+    
     with open(vcf_file, 'r') as f:
         for count, line in enumerate(f, 1):
             # ignore header (lines that start with #)
             if line.rstrip()[0] != '#':
                 # get reference and variant
                 ref = line.rstrip().split('\t')[3]
-                variant = line.rstrip().split('\t')[4]
+                variants = line.rstrip().split('\t')[4]
                 info_elements = line.rstrip().split('\t')[7].split(';')
-                # multiple variants are provided as a list separated by commas
-                variants = variant.split(',')
                 for v in variants:
                     v_size = int()
                     v_type = ''
@@ -60,45 +53,61 @@ if __name__ == "__main__":
                     if len(v) > len(ref):
                         v_size = len(v) - len(ref)
                         v_type = 'insertion'
+                        
                     if v_size >= variant_size_threshold:
                         num_variants += 1
                         accession = GetGeneAccession(v, info_elements)
                         if 'unassigned_transcript_' in accession:
                             accession = 'NA'
+                        # gather information about each indel and add to the summary records
                         if v_type == 'insertion':
-                            ins_out.write(f'{sample}\t{count}\t{v}\t{ref}\t{v_size}\t{accession}\n')
+                            record = {
+                                'Sample': sample,
+                                'Variant': v, 
+                                'Reference': ref,
+                                'Size': v_size,
+                                'Accession': accession,
+                                'Type': 'insertion'
+                            }
+                            summary_records.append(record)
                             insertions.append(v_size)
                         elif v_type == 'deletion':
-                            del_out.write(f'{sample}\t{count}\t{v}\t{ref}\t{v_size}\t{accession}\n')  
+                            record = {
+                                'Sample': sample,
+                                'Variant': v, 
+                                'Reference': ref,
+                                'Size': v_size,
+                                'Accession': accession,
+                                'Type': 'deletion'
+                            }
+                            summary_records.append(record)  
                             deletions.append(v_size)
                 if len(variants) > 1:
                     mul_variants += 1
-    if insertions:
-        print("Min insertion size:", min(insertions))
-    else:
-        print("NO insertions found.")
-    if deletions:
-        print("Min deletion size:", min(deletions))
-    else:
-        print("NO deletions found.")
+    return summary_records
+
+
+if __name__ == "__main__":
+    vcf_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+    num_samples = int(sys.argv[3])
+    variant_size_threshold = int(sys.argv[4])
+
+    # creates full paths for each vcf file in the specified directory
+    vcf_files = [os.path.join(vcf_dir, f) for f in os.listdir(vcf_dir) if f.endswith('.vcf')]
+    # creates the triple variable for each sample's vcf for input into the process sample function
+    tasks = [(vcf, os.path.basename(vcf).replace('.vcf', ''), output_dir) for vcf in vcf_files]
+
+    # use the multiprocessing library tools to run in parallel
+    print(f"Runing {len(vcf_files)} samples using {num_samples} processes")
+    with Pool(processes=num_samples) as pool:
+        results = pool.map(ProcessSample, tasks)
     
-    print(f'Number of multiple variants at a given location: {mul_variants}')
-
-    # save variants size to files
-    with open(os.path.join(output_dir, f'{sample}_deletions_{variant_size_threshold}_count.tsv'), 'w') as f:
-        f.write('\n'.join([str(i) for i in deletions]))
+    all_records = []
+    for records in results:
+        all_records.extend(records)
     
-    with open(os.path.join(output_dir, f'{sample}_insertions_{variant_size_threshold}_count.tsv'), 'w') as f:
-        f.write('\n'.join([str(i) for i in insertions]))
-
-    ## create boxplots
-    #plt.figure(figsize=(8, 6))
-    #ax = sns.boxplot(data=insert_df, x='sample', y='value')
-    #ax.tick_params(axis='x', labelrotation=45)
-    #plt.savefig('insertions_boxplots.png', dpi=300)
-
-    #plt.figure(figsize=(8, 6))
-    #ax = sns.boxplot(data=del_df, x='sample', y='value')
-    #ax.tick_params(axis='x', labelrotation=45)
-    #plt.savefig('deletions_boxplots.png', dpi=300)
-
+    # create combined summary
+    summary_path = os.path.join(output_dir, 'all_samples_cnv_info.tsv')
+    df = pd.DataFrame(all_records)
+    df.to_csv(summary_path, sep='\t', index=False)
