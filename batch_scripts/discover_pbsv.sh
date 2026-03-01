1dcbcb16494a21df13e8d1d9c8907882fe074d47
paytonklein <165186746+paytonklein@users.noreply.github.com>
2026-03-01 17:39:16 -0500

    fixing some syntax that was passing a "$" as a literal argument

--- batch_scripts/discover_pbsv.sh
+++ batch_scripts/discover_pbsv.sh
@@ -26,5 +26,5 @@ BAM="${ALIGN_DIR}/${SAMPLE}.aligned.bam"
 SVSIG="${DISCOVER_DIR}/${SAMPLE}.svsig.gz"
 
 # run pbsv discover to get all of the signatures for the SVs
-pbsv discover $BAM $SVSIG  #--tandem-repeats $TANDEM_REPEATS
-tabix -c '#' -s 3 -b 4 -e 4 $   # optional but recommended if we need to make a pbsv random call (provides indexing for processing efficiency)
+pbsv discover "$BAM" "$SVSIG"  #--tandem-repeats $TANDEM_REPEATS
+tabix -c '#' -s 3 -b 4 -e 4 "$SVSIG"   # optional but recommended if we need to make a pbsv random call (provides indexing for processing efficiency)
