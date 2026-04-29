4772ba061bbe9da861a3118cec2042548ccf3f98
paytonklein <165186746+paytonklein@users.noreply.github.com>
2026-04-29 19:57:55 -0400

    indicate that these are aligned bam files

--- batch_scripts/piranha.sh
+++ batch_scripts/piranha.sh
@@ -17,16 +17,17 @@ cd /scratch4/workspace/payton_klein_uri_edu-CMB320/hg38/STAR_alignments/BAMs
 Piranha \
   -b 20 \
   -s \
+  -a \
   Input_hnRNP-HI.trimmed_Aligned.sortedByCoord.out.bam \
   IP_hnRNP-H1.trimmed_Aligned.sortedByCoord.out.bam \
   > hnRNP-H1_peaks.bed
-
 # -----------------------------
 # IgG control (optional comparison)
 # -----------------------------
 Piranha \
   -b 20 \
   -s \
+  -a \
   Input_IgG.trimmed_Aligned.sortedByCoord.out.bam \
   IP_IgG.trimmed_Aligned.sortedByCoord.out.bam \
   > IgG_peaks.bed
