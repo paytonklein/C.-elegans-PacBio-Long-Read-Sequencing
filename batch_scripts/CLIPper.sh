3961ec5c27e8732fc7782d6476c2e5e0c83eb843
paytonklein <165186746+paytonklein@users.noreply.github.com>
2026-04-28 20:24:39 -0400

    CLIPper slurm

---
+++ batch_scripts/CLIPper.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=clipper
+#SBATCH --output=clipper.out
+#SBATCH --error=clipper.err
+#SBATCH --time=01:00:00
+#SBATCH --mem=8G
+
+module load conda/latest
+conda activate binding_sites
+
+clipper \
+  -b IP_hnRNP-H1.trimmed_Aligned.sortedByCoord.out.bam \
+  -s hg38 \
+  --min-counts 5 \
+  --threshold-method binomial \
+  -o IP_hnRNP-H1_clipper_peaks.bed
+
+clipper \
+  -b IP_IgG.trimmed_Aligned.sortedByCoord.out.bam \
+  -s hg38 \
+  --min-counts 5 \
+  --threshold-method binomial \
+  -o IP_IgG_clipper_peaks.bed
