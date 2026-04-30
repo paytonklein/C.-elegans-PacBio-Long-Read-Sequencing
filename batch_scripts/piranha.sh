44637b0eb9fc0bb58f33eda8615b9a848ea1d063
paytonklein <165186746+paytonklein@users.noreply.github.com>
2026-04-29 20:06:25 -0400

    changes

--- batch_scripts/piranha.sh
+++ batch_scripts/piranha.sh
@@ -3,31 +3,20 @@
 #SBATCH --output=piranha.out
 #SBATCH --error=piranha.err
 #SBATCH --time=02:00:00
-#SBATCH --mem=40G
-#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=2
 
 module load conda/latest
 conda activate piranha_env
 
 cd /scratch4/workspace/payton_klein_uri_edu-CMB320/hg38/STAR_alignments/BAMs
 
-# -----------------------------
-# hnRNP-H1 IP vs Input
-# -----------------------------
-Piranha \
-  -b 20 \
-  -s \
-  -a \
-  Input_hnRNP-HI.trimmed_Aligned.sortedByCoord.out.bam \
-  IP_hnRNP-H1.trimmed_Aligned.sortedByCoord.out.bam \
-  > hnRNP-H1_peaks.bed
-# -----------------------------
-# IgG control (optional comparison)
-# -----------------------------
-Piranha \
-  -b 20 \
-  -s \
-  -a \
-  Input_IgG.trimmed_Aligned.sortedByCoord.out.bam \
-  IP_IgG.trimmed_Aligned.sortedByCoord.out.bam \
-  > IgG_peaks.bed
+Piranha -b 20 -s \
+-c Input_hnRNP-HI.trimmed_Aligned.sortedByCoord.out.bam \
+IP_hnRNP-H1.trimmed_Aligned.sortedByCoord.out.bam \
+> hnRNP-H1_peaks.bed
+
+Piranha -b 20 -s \
+-c Input_IgG.trimmed_Aligned.sortedByCoord.out.bam \
+IP_IgG.trimmed_Aligned.sortedByCoord.out.bam \
+> IgG_peaks.bed
