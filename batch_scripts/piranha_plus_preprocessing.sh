c261af142138e45713152e4b195bdf45700e82a7
paytonklein <165186746+paytonklein@users.noreply.github.com>
2026-05-02 20:14:57 -0400

    j

--- batch_scripts/piranha_plus_preprocessing.sh
+++ batch_scripts/piranha_plus_preprocessing.sh
@@ -15,61 +15,45 @@ pwd
 # -----------------------------
 # Step 1: Filter to standard chromosomes
 # -----------------------------
-echo "starting filter to standard chromosomes"
+echo "Filtering BAMs"
 for f in IP_hnRNP-H1 Input_hnRNP-HI IP_IgG Input_IgG
 do
     samtools view -h ${f}.trimmed_Aligned.sortedByCoord.out.bam \
     | awk '$1 ~ /^@/ || $3 ~ /^chr([0-9]+|X|Y|M)$/' \
     | samtools view -b > ${f}.filtered.bam
 done
-echo "done chromosome filtering w samtools"
 
+# -----------------------------
+# Step 2: BAM → CLEAN BED6 (CRITICAL FIX)
+# -----------------------------
+echo "Converting to clean BED6"
+for f in IP_hnRNP-H1 Input_hnRNP-HI IP_IgG Input_IgG
+do
+    bedtools bamtobed -i ${f}.filtered.bam \
+    | awk 'BEGIN{OFS="\t"} {print $1,$2,$3,".",$5,$6}' \
+    | sort -k1,1 -k2,2n > ${f}.clean.bed
+done
+
+# -----------------------------
+# Switch environment for Piranha
+# -----------------------------
 conda deactivate
 conda activate piranha_env
 echo "Active environment name: $CONDA_DEFAULT_ENV"
 
-echo "start converting from bam -> bed"
-
 # -----------------------------
-# Step 2: Convert BAM → BED
+# Step 3: Run Piranha
 # -----------------------------
-for f in IP_hnRNP-H1 Input_hnRNP-HI IP_IgG Input_IgG
-do
-    bedtools bamtobed -i ${f}.filtered.bam > ${f}.bed
-done
-
-echo "done converting to bed files"
-
-echo "sanity checks: (hopefully return nothing)"
-cut -f1 IP_hnRNP-H1.bed | sort | uniq > ip.chroms
-cut -f1 Input_hnRNP-HI.bed | sort | uniq > input.chroms
-diff ip.chroms input.chroms
-
-echo "sorting bed files" 
-# -----------------------------
-# Step 2.5: Sort BED files (CRITICAL)
-# -----------------------------
-for f in IP_hnRNP-H1 Input_hnRNP-HI IP_IgG Input_IgG
-do
-    sort -k1,1 -k2,2n ${f}.bed > ${f}.sorted.bed
-done
-
-echo "run Piranha for hnRNP-H1"
-# -----------------------------
-# Step 3: Run Piranha (hnRNP-H1)
-# -----------------------------
+echo "Running Piranha (hnRNP-H1)"
 Piranha -b 20 -s -a 0 \
--c Input_hnRNP-HI.sorted.bed \
-IP_hnRNP-H1.sorted.bed \
+-c Input_hnRNP-HI.clean.bed \
+IP_hnRNP-H1.clean.bed \
 > hnRNP-H1_peaks.bed
 
-echo "run Piranha for IgG"
-# -----------------------------
-# Step 4: Run Piranha (IgG)
-# -----------------------------
+echo "Running Piranha (IgG)"
 Piranha -b 20 -s -a 0 \
--c Input_IgG.sorted.bed \
-IP_IgG.sorted.bed \
+-c Input_IgG.clean.bed \
+IP_IgG.clean.bed \
 > IgG_peaks.bed
 
-echo "done!"
+echo "Done"
