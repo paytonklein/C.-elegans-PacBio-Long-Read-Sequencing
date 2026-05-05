4517a5aeda1fcec3adbaf73ac8d2a87d7da3c8b3
paytonklein <165186746+paytonklein@users.noreply.github.com>
2026-05-05 15:48:06 -0400

    fix conda env

--- batch_scripts/macs2.sh
+++ batch_scripts/macs2.sh
@@ -2,30 +2,35 @@
 #SBATCH --job-name=macs2
 #SBATCH --output=macs2.out
 #SBATCH --error=macs2.err
-#SBATCH --time=02:00:00
+#SBATCH --time=04:00:00
 #SBATCH --mem=32G
 
 module load conda/latest
-conda activate binding_sites   # or wherever MACS2 is installed
+
+conda activate macs2_clean_env
+echo "Active environment: $CONDA_DEFAULT_ENV"
 
 cd /scratch4/workspace/payton_klein_uri_edu-CMB320/hg38/STAR_alignments/BAMs
 
-# hnRNP-H1 peak calling
+# -----------------------------
+# Run MACS2 peak calling
+# -----------------------------
+echo "Running MACS2 (hnRNP-H1)"
 macs2 callpeak \
   -t IP_hnRNP-H1.trimmed_Aligned.sortedByCoord.out.bam \
   -c Input_hnRNP-HI.trimmed_Aligned.sortedByCoord.out.bam \
   -f BAM \
   -g hs \
   -n hnRNP-H1 \
-  --outdir macs2_hnRNP-H1 \
-  -q 0.05
+  --outdir macs2_hnRNP-H1
 
-# IgG control (optional)
+echo "Running MACS2 (IgG control)"
 macs2 callpeak \
   -t IP_IgG.trimmed_Aligned.sortedByCoord.out.bam \
   -c Input_IgG.trimmed_Aligned.sortedByCoord.out.bam \
   -f BAM \
   -g hs \
   -n IgG \
-  --outdir macs2_IgG \
-  -q 0.05
+  --outdir macs2_IgG
+
+echo "Done"
